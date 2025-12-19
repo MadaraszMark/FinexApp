4b630cdefec559f5f0ef72ba44dba22775856799
Mark1337 <madaraszmark41@gmail.com>
2025-12-19 10:37:32 +0100

    Initial Commit

---
+++ FinexApp/FinexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinexAppApp.swift
+//  FinexApp
+//
+//  Created by Madarász Márk on 2025. 12. 19..
+//
+
+import SwiftUI
+
+@main
+struct FinexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
